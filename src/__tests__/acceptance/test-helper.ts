e1a027e0f4a6c795cc84b27935937ef4ade6c6cd
Gareth Murphy <garethcmurphy@gmail.com>
2019-09-27 09:34:32 +0200

    update test helper

--- src/__tests__/acceptance/test-helper.ts
+++ src/__tests__/acceptance/test-helper.ts
@@ -35,8 +35,8 @@ export interface AppWithClient {
 export function givenDataset(dataset?: Partial<Dataset>) {
   const data = Object.assign(
     {
-      pid: 'string',
-      name: 'string',
+      pid: '10.10572',
+      name: 'Small-angle scattering of pressurised water',
       size: 3,
       isPublic: true,
       creationDate: '2019-09-27T06:04:21.429Z',
