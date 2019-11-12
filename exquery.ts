f3c0043d09ec7cc8f6e1cabec8f740a24172cd75
Gareth Murphy <garethcmurphy@gmail.com>
2019-11-12 14:57:15 +0100

    add query

--- exquery.ts
+++ exquery.ts
@@ -1,13 +1,15 @@
 import * as request from 'request-promise-native';
 
 (async () => {
-  const baseUrl = 'http://localhost:3000/datasets';
-  const query = {"where" };
-  const encode_query = encodeURIComponent();
-  const queryString = '';
+  const baseUrl = 'http://localhost:3000/datasets?fields=';
+  const query = {
+    where: {and: [{'pressure.value': {gt: 100}}, {sample: 'water'}]},
+  };
+  const queryString =encodeURIComponent(JSON.stringify(query));
   var options = {
     uri: baseUrl + queryString,
   };
+  console.log(options.uri);
 
   const result = await request.get(options);
   console.log(result);
