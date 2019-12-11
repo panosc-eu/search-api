8ad2add594d43c9a77591d26d5bee4646557b95b
Gareth Murphy <garethcmurphy@gmail.com>
2019-12-11 12:26:52 +0100

    reformat query

--- exquery.ts
+++ exquery.ts
@@ -2,11 +2,20 @@ import * as request from 'request-promise-native';
 
 (async () => {
   const baseUrl = 'http://localhost:3000/datasets?filter=';
-  const query = {
+  let query1 = {
     where: {
       and: [{'pressure.value': {gt: 50}}, {'pressure.unit': 'bar'}],
     },
   };
+  let query = {
+    where: {
+      pressure: {
+        value: { gt: 50 },
+        unit: 'bar',
+      },
+    }
+  };
+  console.log(JSON.stringify(query, null, 2));
   const queryString = encodeURIComponent(JSON.stringify(query));
   const options = {
     uri: baseUrl + queryString,
