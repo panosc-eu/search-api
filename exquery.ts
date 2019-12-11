cab8c38229902a7532fa4db7327da08a59861e82
Gareth Murphy <garethcmurphy@gmail.com>
2019-12-11 13:10:20 +0100

    fix conversion to units

--- exquery.ts
+++ exquery.ts
@@ -2,19 +2,19 @@ import * as request from 'request-promise-native';
 
 (async () => {
   const baseUrl = 'http://localhost:3000/datasets?filter=';
-  let query1 = {
+  const query = {
     where: {
-      and: [{'pressure.value': {gt: 50}}, {'pressure.unit': 'bar'}],
+      and: [
+        {
+          'pressure.value': {
+            lt: 150,
+          },
+        },
+        {'pressure.unit': 'bar'},
+      ],
     },
   };
-  let query = {
-    where: {
-      pressure: {
-        value: { gt: 50 },
-        unit: 'bar',
-      },
-    }
-  };
+
   console.log(JSON.stringify(query, null, 2));
   const queryString = encodeURIComponent(JSON.stringify(query));
   const options = {
--- src/repositories/dataset.repository.ts
+++ src/repositories/dataset.repository.ts
@@ -24,30 +24,8 @@ export class DatasetRepository extends DefaultCrudRepository<
           if (Object.prototype.hasOwnProperty.call(whereFilter, 'and')) {
             // console.log('where filter ', whereFilter['and']);
             const andQuery = whereFilter['and'] as Array<Object>;
-            let unit = 'm';
-            let val = '0';
-            andQuery.forEach(element => {
-              console.log('ele', element);
-              Object.entries(element).forEach(entry => {
-                const key = entry[0];
-                const value = entry[1];
-                if (key.endsWith('.unit')) {
-                  console.log('key', key);
-                  unit = value;
-                }
-                if (key.endsWith('.value')) {
-                  console.log(key);
-                  val = value;
-                  val = extractValueFromOperator(value);
-                }
-              });
-            });
-            const qtyString = String(val) + ' ' + unit;
-            console.log(qtyString);
-            const qty = new Qty(qtyString);
-            console.log(new Date(Date.now()));
-            console.log(qty.toString());
-            console.log(qty.toBase().toString());
+            const convertedQuery = convertQuery(andQuery);
+            ctx.query.where = convertedQuery;
           }
         }
       }
@@ -63,4 +41,58 @@ function extractValueFromOperator(operator: Object) {
     // console.log(value2);
   });
   return value;
+}
+
+function convertQuery(andQuery: Array<Object>) {
+  let unit = 'bar';
+  let val = '50';
+  andQuery.forEach(element => {
+    console.log(element);
+
+    console.log('ele', element);
+    Object.entries(element).forEach(entry => {
+      const key = entry[0];
+      const value = entry[1];
+      if (key.endsWith('.unit')) {
+        console.log('key', key);
+        unit = value;
+      }
+      if (key.endsWith('.value')) {
+        console.log(key);
+        val = value;
+        val = extractValueFromOperator(value);
+      }
+    });
+  });
+  const qtyString = String(val) + ' ' + unit;
+  console.log(qtyString);
+  const qty = new Qty(qtyString);
+  console.log(new Date(Date.now()));
+  console.log(qty.toString());
+  console.log(qty.toBase().toString());
+
+  const convertedQuantity = qty.toBase().toString();
+
+  const convertedUnit = convertedQuantity.substr(
+    convertedQuantity.indexOf(' ') + 1,
+  );
+  const convertedValue = convertedQuantity.substr(
+    0,
+    convertedQuantity.indexOf(' '),
+  );
+  console.log('conversion', convertedValue, convertedUnit);
+  const query = {
+    and: [
+      {
+        'pressure.value': {
+          lt: convertedValue,
+        },
+      },
+      {
+        'pressure.unit': convertedUnit,
+      },
+    ],
+  };
+  console.log(JSON.stringify(query, null, 2));
+  return query;
 }
