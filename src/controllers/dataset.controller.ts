2bb89e3602a1bddcecb23cef329762621b6748ce
Gareth Murphy <garethcmurphy@gmail.com>
2020-01-14 13:33:47 +0100

    add services sccat

--- src/controllers/dataset.controller.ts
+++ src/controllers/dataset.controller.ts
@@ -1,189 +1,14 @@
-import {
-  Count,
-  CountSchema,
-  Filter,
-  repository,
-  Where,
-} from '@loopback/repository';
-import {
-  post,
-  param,
-  get,
-  getFilterSchemaFor,
-  getModelSchemaRef,
-  getWhereSchemaFor,
-  patch,
-  put,
-  del,
-  requestBody,
-} from '@loopback/rest';
+import {param, get, getModelSchemaRef} from '@loopback/rest';
 import {Dataset} from '../models';
-import {DatasetRepository} from '../repositories';
 import {inject} from '@loopback/context';
-import {get, param} from '@loopback/rest';
 
 import {Scicat} from '../services';
 
-
 export class DatasetController {
   constructor(
-    @repository(DatasetRepository)
-    public datasetRepository: DatasetRepository,
-    @inject('services.ScicatService'),
-    protected scicatService: Scicat
+    @inject('services.ScicatService')
+    protected scicatService: Scicat,
   ) {}
-
-  @post('/datasets', {
-    responses: {
-      '200': {
-        description: 'Dataset model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Dataset)}},
-      },
-    },
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Dataset, {
-            title: 'NewDataset',
-          }),
-        },
-      },
-    })
-    dataset: Dataset,
-  ): Promise<Dataset> {
-    return this.datasetRepository.create(dataset);
-  }
-
-  @get('/datasets/count', {
-    responses: {
-      '200': {
-        description: 'Dataset model count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async count(
-    @param.query.object('where', getWhereSchemaFor(Dataset))
-    where?: Where<Dataset>,
-  ): Promise<Count> {
-    return this.datasetRepository.count(where);
-  }
-
-  @get('/datasets', {
-    responses: {
-      '200': {
-        description: 'Array of Dataset model instances',
-        content: {
-          'application/json': {
-            schema: {type: 'array', items: getModelSchemaRef(Dataset)},
-          },
-        },
-      },
-    },
-  })
-  async find(
-    @param.query.object('filter', getFilterSchemaFor(Dataset))
-    filter?: Filter<Dataset>,
-  ): Promise<Dataset[]> {
-    return this.datasetRepository.find(filter);
-  }
-
-  @patch('/datasets', {
-    responses: {
-      '200': {
-        description: 'Dataset PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Dataset, {partial: true}),
-        },
-      },
-    })
-    dataset: Dataset,
-    @param.query.object('where', getWhereSchemaFor(Dataset))
-    where?: Where<Dataset>,
-  ): Promise<Count> {
-    return this.datasetRepository.updateAll(dataset, where);
-  }
-
-  @get('/datasets/{id}', {
-    responses: {
-      '200': {
-        description: 'Dataset model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Dataset)}},
-      },
-    },
-  })
-  async findById(@param.path.string('id') id: string): Promise<Dataset> {
-    return this.datasetRepository.findById(id);
-  }
-
-  @get('/datasets/{id}/metadata', {
-    responses: {
-      '200': {
-        description: 'Dataset metadata info',
-        content: { 'application/xml': {}},
-      },
-    },
-  })
-  async metadata(@param.path.string('id') id: string): Promise<String> {
-    const jsonDataset = this.datasetRepository.findById(id);
-    const xml = jsonToXML(jsonDataset);
-    return xml;
-  }
-
-  @patch('/datasets/{id}', {
-    responses: {
-      '204': {
-        description: 'Dataset PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Dataset, {partial: true}),
-        },
-      },
-    })
-    dataset: Dataset,
-  ): Promise<void> {
-    await this.datasetRepository.updateById(id, dataset);
-  }
-
-  @put('/datasets/{id}', {
-    responses: {
-      '204': {
-        description: 'Dataset PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() dataset: Dataset,
-  ): Promise<void> {
-    await this.datasetRepository.replaceById(id, dataset);
-  }
-
-  @del('/datasets/{id}', {
-    responses: {
-      '204': {
-        description: 'Dataset DELETE success',
-      },
-    },
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.datasetRepository.deleteById(id);
-  }
 
   @get('/datasets/query/{text}', {
     responses: {
@@ -197,19 +22,12 @@ export class DatasetController {
       },
     },
   })
-  async getDetails(@param.path.string('text') text: string) : Promise<any> {
-    return await this.callScicat(text);
+  async getDetails(@param.path.string('text') text: string): Promise<any> {
+    console.log('query of = ', text);
+    return this.callScicat(text);
   }
 
-  async callScicat(text: string): Promise<any>{
-    return await this.scicatService.getDetails(text);
+  async callScicat(text: string): Promise<any> {
+    return this.scicatService.getDetails(text);
   }
-}
-
-function jsonToXML(jsonInput: Object) {
-  const xml = `
-  <?xml version="1.0" encoding="ISO-8859-1" ?>
-<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"></xs:schema>
-  `;
-  return xml;
 }
