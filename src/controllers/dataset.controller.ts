39770e1bcfff86321e76003c40f284fec6ce0d09
Gareth Murphy <garethcmurphy@gmail.com>
2020-01-20 17:17:13 +0100

    add filter to skip and limit lb3-4

--- src/controllers/dataset.controller.ts
+++ src/controllers/dataset.controller.ts
@@ -1,22 +1,33 @@
-import {param, get, getModelSchemaRef} from '@loopback/rest';
+import {
+  param,
+  get,
+  getModelSchemaRef,
+  getFilterSchemaFor,
+} from '@loopback/rest';
 import {Dataset} from '../models';
 import {intercept, Interceptor} from '@loopback/core';
 import {inject} from '@loopback/context';
 
 import {Scicat} from '../services';
 import {UnitsInterceptor} from '../interceptors';
+import {Filter} from '@loopback/repository';
 
 const log: Interceptor = async (invocationCtx, next) => {
   console.log('log: before-' + invocationCtx.methodName);
   // Wait until the interceptor/method chain returns
   if (invocationCtx.args) {
     console.log('args:', invocationCtx.args);
-    console.log('ctx',invocationCtx);
+    console.log('ctx', invocationCtx);
   }
   const result = await next();
   console.log('log: after-' + invocationCtx.methodName);
   return result;
 };
+
+
+interface LooseObject {
+  [key: string]: any
+}
 
 export class DatasetController {
   constructor(
@@ -38,21 +49,35 @@ export class DatasetController {
     },
   })
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  async getDetails(@param.path.string('text') text: string ): Promise<any> {
+  async getDetails(
+    @param.path.string('text') text: string,
+    @param.query.object('filter', getFilterSchemaFor(Dataset))
+    filter?: Filter<Dataset>,
+  ): Promise<Dataset[]> {
     console.log('query of = ', text);
-
+    console.log('query of = ', filter);
+    console.log('query of = ', typeof filter);
     const scicatQuery = {limit: '1', skip: '0'};
-    const fieldsQuery = {text: 'string'};
-    scicatQuery['limit'] = '1';
-    scicatQuery['skip'] = '0';
-    fieldsQuery['text'] = 'v20';
+    const fieldsQuery: LooseObject = {};
+    if (typeof filter !== undefined) {
+      console.log('keys ', filter!.fields);
+      console.log('limit ', filter!.limit);
+      const limit = filter!.limit;
+      if ((limit !== undefined) && (typeof limit !== undefined)) {
+        scicatQuery['limit'] = limit!.toString();
+      }
+      const skip = filter!.skip;
+      if ((skip !==  undefined) && (typeof skip !== undefined)) {
+        scicatQuery['skip'] = skip!.toString();
+      }
+    }
 
-    const jsonLimits ='limits='+ encodeURIComponent(
-       JSON.stringify(scicatQuery),
-    );
-    const jsonFields = 'fields='+encodeURIComponent(
-      JSON.stringify(fieldsQuery),
-    );
+    fieldsQuery['creationLocation'] = 'V20';
+
+    const jsonLimits =
+      'limits=' + encodeURIComponent(JSON.stringify(scicatQuery));
+    const jsonFields =
+      'fields=' + encodeURIComponent(JSON.stringify(fieldsQuery));
     const fullQuery = jsonFields + '&' + jsonLimits;
     console.log(fullQuery);
 
