fd1f90d0972c7436a9fc8e3630a0ef7b645d6094
Gareth Murphy <garethcmurphy@gmail.com>
2020-01-22 12:08:47 +0100

    fixed integration tests

--- src/__tests__/acceptance/dataset.controller.integration.ts
+++ src/__tests__/acceptance/dataset.controller.integration.ts
@@ -12,25 +12,7 @@ describe('DatasetController (integration)', () => {
       const controller = new DatasetController(scicatMockService);
 
       const details = await controller.getDetails('nmx', {limit: 1});
-      const expectedValue = [
-        {
-          doi: '03dd9804-1b04-4d36-b0fb-cf66e9891e7d',
-          affiliation: 'ESS',
-          creator: ['Oliver Lohmann'],
-          publisher: 'ESS',
-          publicationYear: 2019,
-          title: 'SANS/Reflectometry',
-          url: '',
-          abstract: 'SANS/Reflectometry',
-          dataDescription: 'V20 data',
-          thumbnail: '',
-          resourceType: 'NeXus HDF5',
-          numberOfFiles: null,
-          sizeOfArchive: null,
-          pidArray: ['20.500.12269/0a269002-83e2-4f18-bb98-36c01836d66a'],
-          authors: ['Oliver Lohmann'],
-        },
-      ];
+
       expect(details).to.be.an.Array();
     });
   });
