e7e020564cd964d8830d8df13feb1b430bab951d
Gareth Murphy <garethcmurphy@gmail.com>
2020-01-27 09:50:37 +0100

    remove unused methods

--- src/controllers/instrument.controller.ts
+++ src/controllers/instrument.controller.ts
@@ -6,16 +6,11 @@ import {
   Where,
 } from '@loopback/repository';
 import {
-  post,
   param,
   get,
   getFilterSchemaFor,
   getModelSchemaRef,
   getWhereSchemaFor,
-  patch,
-  put,
-  del,
-  requestBody,
 } from '@loopback/rest';
 import {Instrument} from '../models';
 import {InstrumentRepository} from '../repositories';
@@ -25,29 +20,6 @@ export class InstrumentController {
     @repository(InstrumentRepository)
     public instrumentRepository: InstrumentRepository,
   ) {}
-
-  @post('/instruments', {
-    responses: {
-      '200': {
-        description: 'Instrument model instance',
-        content: {'application/json': {schema: getModelSchemaRef(Instrument)}},
-      },
-    },
-  })
-  async create(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Instrument, {
-            title: 'NewInstrument',
-          }),
-        },
-      },
-    })
-    instrument: Instrument,
-  ): Promise<Instrument> {
-    return this.instrumentRepository.create(instrument);
-  }
 
   @get('/instruments/count', {
     responses: {
@@ -83,29 +55,6 @@ export class InstrumentController {
     return this.instrumentRepository.find(filter);
   }
 
-  @patch('/instruments', {
-    responses: {
-      '200': {
-        description: 'Instrument PATCH success count',
-        content: {'application/json': {schema: CountSchema}},
-      },
-    },
-  })
-  async updateAll(
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Instrument, {partial: true}),
-        },
-      },
-    })
-    instrument: Instrument,
-    @param.query.object('where', getWhereSchemaFor(Instrument))
-    where?: Where<Instrument>,
-  ): Promise<Count> {
-    return this.instrumentRepository.updateAll(instrument, where);
-  }
-
   @get('/instruments/{id}', {
     responses: {
       '200': {
@@ -116,51 +65,5 @@ export class InstrumentController {
   })
   async findById(@param.path.string('id') id: string): Promise<Instrument> {
     return this.instrumentRepository.findById(id);
-  }
-
-  @patch('/instruments/{id}', {
-    responses: {
-      '204': {
-        description: 'Instrument PATCH success',
-      },
-    },
-  })
-  async updateById(
-    @param.path.string('id') id: string,
-    @requestBody({
-      content: {
-        'application/json': {
-          schema: getModelSchemaRef(Instrument, {partial: true}),
-        },
-      },
-    })
-    instrument: Instrument,
-  ): Promise<void> {
-    await this.instrumentRepository.updateById(id, instrument);
-  }
-
-  @put('/instruments/{id}', {
-    responses: {
-      '204': {
-        description: 'Instrument PUT success',
-      },
-    },
-  })
-  async replaceById(
-    @param.path.string('id') id: string,
-    @requestBody() instrument: Instrument,
-  ): Promise<void> {
-    await this.instrumentRepository.replaceById(id, instrument);
-  }
-
-  @del('/instruments/{id}', {
-    responses: {
-      '204': {
-        description: 'Instrument DELETE success',
-      },
-    },
-  })
-  async deleteById(@param.path.string('id') id: string): Promise<void> {
-    await this.instrumentRepository.deleteById(id);
   }
 }
