a0451dec103167e24573237a78bedfd4566c0415
Gareth Murphy <garethcmurphy@gmail.com>
2020-02-18 11:24:19 +0100

    reformat query

--- exampleQuery.ts
+++ exampleQuery.ts
@@ -5,7 +5,14 @@ import * as request from 'request-promise-native';
   const query1 = {
     limit: 1,
     skip: 0,
-    include: {relation: 'samples', scope: {where: {description: 'xx'}}},
+    include: {
+      relation: 'samples',
+      scope: {
+        where: {
+          description: 'xx',
+        },
+      },
+    },
     where: {
       and: [
         {
