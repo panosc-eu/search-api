8212e61ed700eeac2573b0c47f65d20ff764d025
Henrik Johansson <henrik.johansson712@gmail.com>
2020-05-06 09:07:22 +0200

    Updated git workflow + added document members relation

--- common/models/document.js
+++ common/models/document.js
@@ -1,5 +1,11 @@
 'use strict';
 
-module.exports = function(Document) {
-
+module.exports = function (Document) {
+  Document.disableRemoteMethodByName('prototype.__get__members');
+  Document.disableRemoteMethodByName('prototype.__create__members');
+  Document.disableRemoteMethodByName('prototype.__delete__members');
+  Document.disableRemoteMethodByName('prototype.__updateById__members');
+  Document.disableRemoteMethodByName('prototype.__findById__members');
+  Document.disableRemoteMethodByName('prototype.__destroyById__members');
+  Document.disableRemoteMethodByName('prototype.__count__members');
 };
