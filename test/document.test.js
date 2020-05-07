b98b48efe780e1a63102aa6df14df44c40ac69c7
Henrik Johansson <henrik.johansson712@gmail.com>
2020-05-07 08:09:02 +0200

    Added document test

--- test/document.test.js
+++ test/document.test.js
@@ -34,6 +34,67 @@ describe('Document', () => {
           });
       });
     });
+
+    context(
+      'where type is proposal and the principal investigator is a specific person',
+      () => {
+        it('should return an array of documents mathing the type and the person', (done) => {
+          const filter = JSON.stringify({
+            where: {
+              type: 'proposal',
+            },
+            include: [
+              {
+                relation: 'datasets',
+              },
+              {
+                relation: 'members',
+                scope: {
+                  where: {
+                    role: 'principal investigator',
+                  },
+                  include: [
+                    {
+                      relation: 'person',
+                      scope: {
+                        where: {
+                          fullName: 'James Chadwick',
+                        },
+                      },
+                    },
+                  ],
+                },
+              },
+            ],
+          });
+          request(app)
+            .get(requestUrl + '?filter=' + filter)
+            .set('Accept', 'application/json')
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+              if (err) throw err;
+
+              expect(res.body).to.be.an('array');
+              res.body.forEach((document) => {
+                expect(document).to.have.property('pid');
+                expect(document).to.have.property('isPublic');
+                expect(document).to.have.property('type');
+                expect(document.type).to.equal('proposal');
+                expect(document).to.have.property('title');
+                expect(document).to.have.property('datasets');
+                expect(document.datasets).to.be.an('array').and.not.empty;
+                expect(document).to.have.property('members');
+                document.members.forEach((member) => {
+                  expect(member.role).to.equal('principal investigator');
+                  expect(member.person.fullName).to.equal('James Chadwick');
+                });
+              });
+              done();
+            });
+        });
+      },
+    );
   });
 
   describe('GET /documents/{id}', () => {
