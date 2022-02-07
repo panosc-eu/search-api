21f681424f939868ed7496fc182611ec7e57bc6b
minotti_c <carlo.minotti@psi.ch>
2022-02-07 19:17:48 +0100

    Add panet mixin

---
+++ common/mixins/panet.js
@@ -0,0 +1,69 @@
+"use strict";
+
+const superagent = require("superagent");
+
+class Panet {
+
+  constructor(baseUrl) {
+    this.panetUrl = baseUrl + "/techniques/pan-ontology";
+  }
+
+  /**
+   * request panet ontology to pan-ontologies service
+   * @param {object} techniqueLoopbackWhere Loopback where filter with condition
+   *  on a field of the technique model
+   * @returns {object} Loopback where filter made of condition on list of pids:
+   * e.g {and:[{pid:{inq: [1,2,3]}, {pid: {inq: [4,5,6]}}]}
+   * or {or:[{pid:{inq: [1,2,3]}, {pid: {inq: [4,5,6]}}]}
+   * or {pid:{inq: [1,2,3]}}
+   */
+  async panet(techniqueLoopbackWhere) {
+
+    console.log(">>> Panet.panet: panet requested");
+    console.log(" - where filter : ", techniqueLoopbackWhere);
+
+    const res = await superagent
+      .get(this.panetUrl)
+      .query({ where: JSON.stringify(techniqueLoopbackWhere) });
+    const resJSON = JSON.parse(res.text);
+    await deepObjectSearchAndReplace(resJSON,
+      "pid",
+      () => true,
+      (obj) => (obj.panetId = obj.pid, delete obj.pid)
+    );
+    return resJSON
+  }
+
+}
+
+async function deepObjectSearchAndReplace (object, key, predicate, callable) {
+  if(object.hasOwnProperty(key) && predicate(object[key]) === true)
+      await callable(object);
+
+  for(var i=0; i<Object.keys(object).length; i++){
+      if(typeof object[Object.keys(object)[i]] === "object"){
+          await deepObjectSearchAndReplace(
+            object[Object.keys(object)[i]],
+            key, predicate, callable);
+      }
+  }
+}
+
+
+module.exports = (Model) => {
+  const PanetOntology = process.env.PANET_BASE_URL ?
+  new Panet(process.env.PANET_BASE_URL) :
+  { panet: (techniqueLoopbackWhere) => techniqueLoopbackWhere };
+
+  Model.beforeRemote("find", async (ctx) => {
+    if (ctx.args.filter)
+  await deepObjectSearchAndReplace(
+    ctx.args.filter,
+    "relation",
+    (v) => v === "techniques",
+    async (obj) => (
+      obj.scope.where = await PanetOntology.panet(obj.scope.where))
+  )
+})
+
+}
